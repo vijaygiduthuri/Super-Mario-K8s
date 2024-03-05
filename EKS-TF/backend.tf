9d14dad589cfada4fc859d1fb833b732a0c598ae
Vijay <vijaygiduthuri67@gmail.com>
2024-03-05 14:38:35 +0530

    mario

---
+++ EKS-TF/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "s3bucketforeksterraform"   # Replace with your actual S3 bucket name
+    key    = "EKS/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
